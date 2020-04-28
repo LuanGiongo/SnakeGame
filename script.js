eb8996e8859aa4e06ea00798ce6140666e3f4a92
Luan Giongo <luangiongo@gmail.com>
2020-04-28 19:08:54 -0300

    Criação da captura de teclas

--- script.js
+++ script.js
@@ -21,6 +21,15 @@ function snakeBody() {
   }
 }
 
+document.addEventListener("keydown", update);
+
+function update (event){
+  if(event.keyCode === 37 && direction != "right") direction = "left";
+  if(event.keyCode === 38 && direction != "down")  direction = "up";
+  if(event.keyCode === 39 && direction != "left")  direction = "right";
+  if(event.keyCode === 40 && direction != "up")  direction = "down";
+}
+
 function startGame() {
     background();
     snakeBody();
@@ -28,10 +37,10 @@ function startGame() {
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(direction == "right") snakeX += box;
-    if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY -= box;
-    if(direction == "down") snakeY += box;
+    if(direction === "right") snakeX += box;
+    if(direction === "left") snakeX -= box;
+    if(direction === "up") snakeY -= box;
+    if(direction === "down") snakeY += box;
 
     snake.pop();
 
